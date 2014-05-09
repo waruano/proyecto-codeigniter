9cda42e8b4349a8fda8cc6b5afe7a2d775ab7efb
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-09 05:14:49 +0000

    Fabian, Template base

--- trunk/GestionClientes/application/views/templates/header.php
+++ trunk/GestionClientes/application/views/templates/header.php
@@ -1,6 +1,36 @@
 <html>
 <head>
-<title><?php echo $title ?> - Tutorial de CodeIgniter 2</title>
+<title><?php echo $title ?> - Previmed</title>
+    <link href="images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-<h4>This is Header</h4>
+    <div class="container">       
+        <div>&nbsp;</div>
+        <div class="row" >
+           <div class="col-md-4">
+               <img src="images/logo.png"></img>               
+           </div>
+           <div class="col-md-8">
+               <ul class="nav nav-justified nav-pills">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+               
+           </div>
+       </div>
+      <div class="masthead">        
+                
+      </div>
+
+
+    
