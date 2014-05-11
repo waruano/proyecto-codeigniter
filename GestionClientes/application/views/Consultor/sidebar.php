089ba009616fcd9bb6f865672bdf4f260a7626a7
waruano9212@gmail.com <waruano9212@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-11 04:25:22 +0000

    CRUD Base

---
+++ GestionClientes/application/views/Consultor/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of sidebar
+ *
+ * @author waruano
+ */
+class sidebar {
+    //put your code here
+}
+
+?>
