54372bdccfafcf409b80a906eb8a7fa941a432b3
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-14 06:49:49 +0000

    Fabian, Ajustes minimos navegacion

--- GestionClientes/application/views/Administrador/sidebar.php
+++ GestionClientes/application/views/Administrador/sidebar.php
@@ -1,4 +1,5 @@
 <ul class="nav nav-justified nav-pills">
-    <li class="active"><a href="<?php echo base_url()?>">Home</a></li>
+    <li class="active"><a href="<?php echo base_url()?>">Inicio</a></li>
     <li><?php echo anchor('administrador/Usuarios','Usuarios'); ?></li>
+    <li><?php echo anchor('administrador/planes','Planes    '); ?></li>
 </ul>
