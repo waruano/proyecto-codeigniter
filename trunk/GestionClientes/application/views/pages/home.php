b3c59ef6d28c54e5c56e9a6c382c661a1edb669c
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-14 06:50:24 +0000

    Fabian, Ajustes minimos navegacion

--- trunk/GestionClientes/application/views/pages/home.php
+++ trunk/GestionClientes/application/views/pages/home.php
@@ -1,3 +1,7 @@
 <div>
-    <h3>Welcome to Codeigniter</h3>
+    <h3>Bienvenido al sistema de administración de "PREVIMED"</h3>
+    <p>
+        Mediante el presente sistema podrá administrar información de Titulares, Beneficiarios, Contratos, Papelería y Cartera de <strong>Previmed</strong>. 
+        Es necesario que cuente con un usuario y contraseña suministrado por el administrador de la aplicación.
+    </p>
 </div>
