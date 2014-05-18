90bad1e660cc2ddb27fd58bcb11ac0ea13c6c8e9
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-18 22:34:07 +0000

    Fabian, consulta contratos

--- trunk/GestionClientes/application/views/Administrador/sidebar.php
+++ trunk/GestionClientes/application/views/Administrador/sidebar.php
@@ -3,4 +3,5 @@
     <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/Usuarios','Usuarios'); ?></li>
     <li <?php if(isset($selectedoption) && $selectedoption == 2){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/planes','Planes    '); ?></li>
     <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Titulares');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 4){ echo 'class="active"'; } ?> ><?php echo anchor('consultor/consultar','Consulta Contratos');?>
 </ul>
