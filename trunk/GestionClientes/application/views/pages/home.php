f67475d24ca18e7311e7f3560696659fe81bcaf7
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-18 14:16:30 +0000

    Fabian, Ajustes minimos navegacion

--- trunk/GestionClientes/application/views/pages/home.php
+++ trunk/GestionClientes/application/views/pages/home.php
@@ -1,7 +1,13 @@
 <div>
+    <div>
     <h3>Bienvenido al sistema de administración de "PREVIMED"</h3>
     <p>
         Mediante el presente sistema podrá administrar información de Titulares, Beneficiarios, Contratos, Papelería y Cartera de <strong>Previmed</strong>. 
         Es necesario que cuente con un usuario y contraseña suministrado por el administrador de la aplicación.
     </p>
+    <p>
+        <?php if ($this->tank_auth->is_logged_in() != true) {	
+                 echo anchor('auth', 'Ingresar');        } ?>
+    </p>
+    </div>    
 </div>
