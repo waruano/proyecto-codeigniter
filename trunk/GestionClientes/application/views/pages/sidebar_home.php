5f3b5d1acc3a9bd457498a282e22580f9aef2953
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-18 14:16:14 +0000

    Fabian, Ajustes minimos navegacion

--- trunk/GestionClientes/application/views/pages/sidebar_home.php
+++ trunk/GestionClientes/application/views/pages/sidebar_home.php
@@ -1,4 +1,1 @@
-<ul class="nav nav-justified nav-pills">    
-    <li class="active"><?php echo anchor('home', 'Inicio'); ?></li>    
-    <li><?php echo anchor('auth', 'Ingresar'); ?></li>
-</ul>
+<p>&nbsp;</p>
