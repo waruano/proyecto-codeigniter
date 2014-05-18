2bad9a606c6cb0c16baf386bed6d83b4325ea6ce
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-18 20:36:41 +0000

    Fabian, contactos y ajustes menu

--- trunk/GestionClientes/application/views/Administrador/sidebar.php
+++ trunk/GestionClientes/application/views/Administrador/sidebar.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-justified nav-pills">
-    <li class="active"><a href="<?php echo base_url()?>">Inicio</a></li>
-    <li><?php echo anchor('administrador/Usuarios','Usuarios'); ?></li>
-    <li><?php echo anchor('administrador/planes','Planes    '); ?></li>
-    <li><?php echo anchor('contratos','Titulares');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 0){ echo 'class="active"'; } ?> ><a href="<?php echo base_url()?>">Inicio</a></li>
+    <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/Usuarios','Usuarios'); ?></li>
+    <li <?php if(isset($selectedoption) && $selectedoption == 2){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/planes','Planes    '); ?></li>
+    <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Titulares');?>
 </ul>
