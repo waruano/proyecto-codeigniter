5f006e425ada93e50a331e1a06e296ccc2fa62d4
waruano9212@gmail.com <waruano9212@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-19 00:49:16 +0000

    crud de contratos sin "agregar titular"

--- GestionClientes/application/views/Administrador/sidebar.php
+++ GestionClientes/application/views/Administrador/sidebar.php
@@ -1,7 +1,9 @@
 <ul class="nav nav-justified nav-pills">
     <li <?php if(isset($selectedoption) && $selectedoption == 0){ echo 'class="active"'; } ?> ><a href="<?php echo base_url()?>">Inicio</a></li>
-    <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/Usuarios','Usuarios'); ?></li>
+    <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/usuarios','Usuarios'); ?></li>
     <li <?php if(isset($selectedoption) && $selectedoption == 2){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/planes','Planes    '); ?></li>
-    <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Titulares');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('contratos/titulares','Titulares');?>
     <li <?php if(isset($selectedoption) && $selectedoption == 4){ echo 'class="active"'; } ?> ><?php echo anchor('consultor/consultar','Consulta Contratos');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 4){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Contratos');?>
+
 </ul>
