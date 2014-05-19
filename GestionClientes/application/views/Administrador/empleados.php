0818142bcdc366c8c6e45135d1c7691f6d8bb569
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-19 04:36:19 +0000

    Ajustes apariencia

---
+++ GestionClientes/application/views/Administrador/empleados.php
@@ -0,0 +1,14 @@
+<?php foreach ($css_files as $file): ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+
+<?php endforeach; ?>
+<?php foreach ($js_files as $file): ?>
+
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+<div>
+    <h3>Empleados</h3>
+    <?php
+    echo $output;
+    ?>
+</div>
--- GestionClientes/application/views/Administrador/sidebar.php
+++ GestionClientes/application/views/Administrador/sidebar.php
@@ -4,6 +4,8 @@
     <li <?php if(isset($selectedoption) && $selectedoption == 2){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/planes','Planes    '); ?></li>
     <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('contratos/titulares','Titulares');?>
     <li <?php if(isset($selectedoption) && $selectedoption == 4){ echo 'class="active"'; } ?> ><?php echo anchor('consultor/consultar','Consulta Contratos');?>
-    <li <?php if(isset($selectedoption) && $selectedoption == 4){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Contratos');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 5){ echo 'class="active"'; } ?> ><?php echo anchor('contratos','Contratos');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 6){ echo 'class="active"'; } ?> ><?php echo anchor('digitador/documentos','Documentos');?>
+    <li <?php if(isset($selectedoption) && $selectedoption == 7){ echo 'class="active"'; } ?> ><?php echo anchor('administrador/empleados','Empleados');?>
 
 </ul>
