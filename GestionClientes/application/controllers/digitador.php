50c9c3f1f7d84eee278c6a8306d01ee8f42be5ec
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-23 18:45:07 +0000

    CRUD pagos

--- GestionClientes/application/controllers/digitador.php
+++ GestionClientes/application/controllers/digitador.php
@@ -29,7 +29,9 @@ class digitador extends CI_Controller {
         $crud->display_as('EMPID', 'Asignado a');
         $crud->display_as('NUMERO', 'Número');
         $crud->display_as('TIPO', 'Tipo');
-        $crud->set_relation('EMPID','Persona','{Nombres} {Apellidos}', array('TIPOPERSONA' => '2'));        
+        
+        $crud->set_relation('EMPID','Persona','{Nombres} {Apellidos}', array('TIPOPERSONA' => '2'));  
+        
         $crud->edit_fields('EMPID', 'NUMERO', 'TIPO', 'ESTADO');
         $crud->required_fields('NUMERO', 'TIPO', 'ESTADO');
         $crud->add_fields('EMPID', 'NUMERO', 'TIPO', 'ESTADO');        
@@ -48,4 +50,41 @@ class digitador extends CI_Controller {
         $this->template->render();
     }
 
+    function pagos()
+    {
+        //informacion de Usuario
+        $data['user_id'] = $this->tank_auth->get_user_id();
+        $data['username'] = $this->tank_auth->get_username();
+        $data['selectedoption'] = 8;
+        //Configuracion Grocery_CRUD listado de usuarios
+        $crud = new Grocery_CRUD();
+        $crud->set_table('pago');
+        $crud->set_subject("Pagos");
+        $crud->columns('RECID', 'TITID', 'VALOR', 'FECHA');
+        $crud->display_as('RECID', 'Recibo de caja');
+        $crud->display_as('TITID', 'Titular de contrato');
+        $crud->display_as('VALOR', 'Valor pagado');
+        $crud->display_as('FECHA', 'Fecha de pago');
+        
+        $crud->set_relation('RECID','Documento','{Numero}', array('Estado' => '1'));        
+        $crud->set_relation('TITID','Persona','{Nombres} {Apellidos}', array('TIPOPERSONA' => '0'));        
+        
+        $crud->edit_fields('RECID', 'TITID', 'VALOR', 'FECHA');
+        $crud->required_fields('RECID', 'TITID', 'VALOR', 'FECHA');
+        $crud->add_fields('RECID', 'TITID', 'VALOR', 'FECHA');
+        
+        $crud->field_type('FECHAHASTA', 'date');
+        $crud->field_type('VALOR', 'integer');
+        
+        //$crud->add_action('Costos', '', 'Administrador/costosplan');
+        //$crud->add_action('Tarifas', base_url() . 'images/money.png', 'Costos','',array($this,'direccion_planes'));
+        $output = $crud->render();
+
+        //Configuracion de la Plantilla
+        $this->template->write_view('login', $this->tank_auth->get_login(), $data);
+        $this->template->write('title', 'Planes');
+        $this->template->write_view('sidebar', $this->tank_auth->get_sidebar());
+        $this->template->write_view('content', 'digitador/pagos', $output);
+        $this->template->render();
+    }
 }
