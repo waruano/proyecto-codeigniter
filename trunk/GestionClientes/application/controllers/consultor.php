26e38322aa5f6eac24588d425d0cf264238c3bdb
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-05-28 05:39:45 +0000

    Ajustes CRUD Beneficiarios, ajustes interfaz, consulta contratos y detalle de la misma

--- trunk/GestionClientes/application/controllers/consultor.php
+++ trunk/GestionClientes/application/controllers/consultor.php
@@ -34,10 +34,10 @@ class Consultor extends CI_Controller {
         $crud->set_model('Custom_query_model');
         $crud->set_table('persona'); //Change to your table name
         
-        $strSQL = "select 'Estado' as Estado, documento.Numero as NumeroContrato, CONCAT(persona.Nombres, ' ', persona.Apellidos) as NombreTitular, 
-            persona.NoDocumento as Identificacion, contrato.FechaInicio as FechaAfiliacion, documento.Numero as ID
-            from persona left join contrato on contrato.TITID = persona.ID left join documento on documento.ID = contrato.DOCID
-            where contrato.ESTADO = 0";
+        $strSQL = "select 'Estado' as Estado, documento.Numero as NumeroContrato, CONCAT(titular.Nombres, ' ', ifnull(titular.Apellidos, '')) as NombreTitular, 
+            titular.NoDocumento as Identificacion, contrato.FechaInicio as FechaAfiliacion, titular.id as ID
+            from titular left join contrato on contrato.TITID = titular.ID left join documento on documento.ID = contrato.DOCID
+            where contrato.ESTADO = 1";
        
         if($data['valtitular'] != '' )
         {
@@ -61,7 +61,7 @@ class Consultor extends CI_Controller {
         $crud->unset_delete();
         $crud->unset_read();
         $crud->add_action('Detalles', base_url() . 'images/magnifier.png', 'Detalles','',array($this,'direccion_contratos'));
-        $crud->unset_jquery();
+        $crud->unset_jquery();       
         $output = $crud->render();
         
         //Configuracion de la Plantilla
@@ -74,6 +74,77 @@ class Consultor extends CI_Controller {
     
     function direccion_contratos($primary_key , $row)
     {
-        return 'costosplan/' . $primary_key;
+        return base_url() . 'consultor/detalleTitulares/' . $primary_key;
+    }
+    
+    
+    function detalleTitulares($noContrato)
+    {
+        //informacion de Usuario
+        $data['user_id'] = $this->tank_auth->get_user_id();
+        $data['username'] = $this->tank_auth->get_username();
+        $data['selectedoption'] = 4;  
+        
+        $valTitId = $noContrato;
+        $query = $this->db->query("SELECT *
+                                   FROM titular Left Join CONTRATO on Contrato.TitId = titular.ID   and contrato.estado = 1
+                                   left join documento on documento.id = contrato.docId   WHERE titular.id = " . $valTitId);
+        if ($query->num_rows() > 0) {
+            $row = $query->row(0);
+            $data['titular'] = $row;
+            
+        } else {
+            $data['titular']  = null;
+        }
+        
+        /// Se consultan los beneficiarios del titular
+        $qbeneficiarios = $this->db->query("SELECT * FROM BENEFICIARIO WHERE titId = " . $valTitId );
+        if($qbeneficiarios->num_rows() > 0 )
+        {
+            $data['beneficiarios']  = $qbeneficiarios->result() ;
+            $data['tienebeneficiarios'] = true;
+        }
+        else {
+            $data['tienebeneficiarios'] = false;
+        }
+        
+        /// Se consultan los contactos del titular
+        $qcontactos = $this->db->query("SELECT * FROM CONTACTO WHERE titId = " . $valTitId );
+        if($qcontactos->num_rows() > 0 )
+        {
+            $data['contactos']  = $qcontactos->result() ;        
+            $data['tienecontactos'] = true;
+        }
+        else {
+            $data['tienecontactos'] = false;
+        }
+        
+        /// Se consulta la informacion del contrato y los pagos realizados por el titular desde la vigencia del contrato
+        $qcontrato = $this->db->query("SELECT CONTRATO.FECHAINICIO, PLAN.NOMBRE, PLAN.FORMAPAGO, PLAN.PERIODICIDAD, PLAN.TIPOPLAN, 
+                                        PLAN.NOMBRECONVENIO, DOCUMENTO.NUMERO, DOCUMENTO.TIPO
+                                        FROM CONTRATO INNER JOIN PLAN ON PLAN.ID = CONTRATO.PLANID 
+                                        INNER JOIN DOCUMENTO ON DOCUMENTO.ID = CONTRATO.DOCID WHERE contrato.ESTADO = 1 AND TITID = " .$valTitId );
+        if($qcontrato->num_rows() > 0 )
+        {
+            $contrato = $query->row(0);
+            $data['contrato'] = $contrato;
+            $data['tienecontrato'] = true;
+            
+            $qpagos = $this->db->query("SELECT * FROM PAGO WHERE TITID = " . $valTitId . " AND FECHA >= " . $contrato->FECHAINICIO);
+            
+        }
+        else
+        {
+            $data['tienecontrato'] = false;
+        }
+        
+        //Configuracion de la Plantilla
+        $this->template->write_view('login', $this->tank_auth->get_login(), $data);
+        $this->template->write('title', 'Búsqueda de Contratos');
+        $this->template->write_view('sidebar', $this->tank_auth->get_sidebar());
+        $this->template->write_view('content', 'consultor/detalleTitulares');
+        $this->template->render();
     }
 }
+
+
