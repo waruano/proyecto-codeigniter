2341d359921c30bcc546725f7a652a2acb497b7d
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-06-03 05:31:54 +0000

    Consulta contratos, ajustes visuales

--- trunk/GestionClientes/application/views/pages/login_page.php
+++ trunk/GestionClientes/application/views/pages/login_page.php
@@ -1,13 +1,5 @@
 <br/><br/>
-<div>
-    <div>
-    <h3>Bienvenido al sistema de administración de "PREVIMED"</h3>
-    <p>
-        Mediante el presente sistema podrá administrar información de Titulares, Beneficiarios, Contratos, Papelería y Cartera de <strong>Previmed</strong>. 
-        Es necesario que cuente con un usuario y contraseña suministrado por el administrador de la aplicación.
-    </p>
-    </div>
-</div>
+<?php $this->load->view('pages/initpage')  ?>
 <?php
 $login = array(
 	'name'	=> 'login',
