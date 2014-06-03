28dd5c295cc176a6192fbb2221e1f364fd6d1c74
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-06-03 05:31:54 +0000

    Consulta contratos, ajustes visuales

--- GestionClientes/application/views/pages/home.php
+++ GestionClientes/application/views/pages/home.php
@@ -1,11 +1,16 @@
 <?php if ($this->tank_auth->is_logged_in() != true) {	?><br/><br/><?php  } ?>
 <div>
     <div>
-    <h3>Bienvenido al sistema de administración de "PREVIMED"</h3>
+<!--    <h3>Bienvenido al sistema de administración de "PREVIMED"</h3>
     <p>
         Mediante el presente sistema podrá administrar información de Titulares, Beneficiarios, Contratos, Papelería y Cartera de <strong>Previmed</strong>. 
         Es necesario que cuente con un usuario y contraseña suministrado por el administrador de la aplicación.
     </p>
+    -->
+    
+   
+    <?php $this->load->view('pages/initpage')  ?>
+    
     <p>
         <?php if ($this->tank_auth->is_logged_in() != true) {	
                  echo anchor('auth', 'Ingresar');        } ?>
---
+++ GestionClientes/application/views/pages/initpage.php
@@ -0,0 +1,77 @@
+ <div class="row"  >
+    <div class="col-md-6">
+        <img src="<?php echo base_url() . 'images/presentacion.png' ?>" />
+    </div>
+    <div class="col-md-6">
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                    Evite desplazamientos
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseOne" class="panel-collapse collapse in">
+                  <div class="panel-body">
+                    No pierda tiempo! <br/>
+                      Nuestro servicio de médico en casa le garantiza optima respuesta.
+                      Sin salir de casa!<br/>
+                      Llevamos profesionales altamente capacitados a la puerta de su casa para atender sus necesidades en salud.<br/>
+                  </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                    Sin límites de visitas
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseTwo" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    Pensando en usted<br/>
+                    Hemos dispuesto un amplio número de funcionarios que velaran por su salud.<br/>
+                    Cuantas veces lo requiera<br/>
+                    Nuestros profesionals visitarán su hogar :)<br/>
+                  </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseTree">
+                    Evite largas esperas en hospitales
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseTree" class="panel-collapse collapse ">
+                  <div class="panel-body">
+                    Somos diferentes?<br/>
+                    Nuestros servicios son especiales para usted y le evitaran molestas esperas.<br/>
+                    Sin esperar horas y horas<br/>
+                    Cambiamos el concépto de salud para enfocarlo en el bienestar de los pacientes!<br/>
+                  </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                    No tiene límites de edad
+                  </a>
+                </h4>
+              </div>
+              <div id="collapseFour" class="panel-collapse collapse ">
+                  <div class="panel-body">
+                   Sin complicaciones!<br/>
+                    Cubrimos con nuestros servicios a toda la familia, especialmente a niños y ancianos<br/>
+                    100% Profesionales<br/>
+                    Dispuestos para atender cualquier eventualidad.<br/>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
