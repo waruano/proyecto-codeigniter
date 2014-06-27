5f6263075df22eddf1101a705acff7778d23351a
ruano504@gmail.com <ruano504@gmail.com@2e3ddcd4-7cc3-4a30-5efa-38d4cf241b50>
2014-06-27 05:29:12 +0000

    Ajustes para publicacion

--- trunk/GestionClientes/application/views/Administrador/sidebar.php
+++ trunk/GestionClientes/application/views/Administrador/sidebar.php
@@ -1,7 +1,7 @@
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav nav-justified nav-pills">
         <li <?php if(isset($selectedoption) && $selectedoption == 0){ echo 'class="active"'; } else echo 'class="menuli"'; ?> ><a href="<?php echo base_url()?>">Inicio</a></li>        
-        <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('home/remap/Administrador/Usuarios','Usuarios'); ?></li>
+        <li <?php if(isset($selectedoption) && $selectedoption == 1){ echo 'class="active"'; } ?> ><?php echo anchor('home/remap/administrador/usuarios','Usuarios'); ?></li>
         <li <?php if(isset($selectedoption) && $selectedoption == 7){ echo 'class="active"'; } ?> ><?php echo anchor('home/remap/administrador/empleados','Empleados');?></li>
         <li <?php if(isset($selectedoption) && $selectedoption == 2){ echo 'class="active"'; } ?> ><?php echo anchor('home/remap/administrador/planes','Planes    '); ?></li>           
         <li <?php if(isset($selectedoption) && $selectedoption == 3){ echo 'class="active"'; } ?> ><?php echo anchor('home/remap/contratos/titulares','Titulares');?></li>    
